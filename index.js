348968dc492c1ab656b6cb1a21758bc1659711f6
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-05 00:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var recipes = {key:'value'};
 
 function updateObjectWithKeyAndValue(recipes, key, value) {
-  return Object.assign({}, recipes, { [key]: value })
+  return Object.assign ({}, recipes,{key: value})
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
-  return recipes[key] = value
+ recipes[key] = value
+ return recipes
 }
